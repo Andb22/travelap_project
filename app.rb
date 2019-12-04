d06118ea94493c1614446feda5edfb4083baa4a8
Andrew Binnie <binnieandrew@gmail.com>
2019-12-04 11:33:47 +0000

    adding correct controller names

--- app.rb
+++ app.rb
@@ -5,9 +5,9 @@ require_relative('models/city')
 require_relative('models/site')
 also_reload('./models/*')
 
-require_relative('./controllers/city_controller')
-require_relative('./controllers/country_controller')
-require_relative('./controllers/site_controller')
+require_relative('./controllers/cities_controller')
+require_relative('./controllers/countries_controller')
+require_relative('./controllers/sites_controller')
 
 #homepage
 get '/' do
